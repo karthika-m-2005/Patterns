639cb9b6df2e162cc8557e09f89cd1dc961cbbb0
KARTHIKA M <km646383@gmail.com>
2025-03-23 19:06:12 +0530

    Create NumberPattern.java

---
+++ NumberPattern.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class NumberPattern
+{
+public static void main(String[] args)
+{
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+
+for(int i = 1;i<=n;i++)
+{
+for(int j = n;j<1;j--)
+{
+System.out.print(i);
+}
+System.out.println();
+}
+sc.close();
+}
+}
+
