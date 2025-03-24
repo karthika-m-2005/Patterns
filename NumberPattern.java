32dd36ae1af253667d00f7d2d62e84608083ea8d
KARTHIKA M <km646383@gmail.com>
2025-03-24 22:59:49 +0530

    Update NumberPattern.java

--- NumberPattern.java
+++ NumberPattern.java
@@ -4,6 +4,7 @@ public class NumberPattern
 public static void main(String[] args)
 {
 Scanner sc = new Scanner(System.in);
+System.out.println("Enter the Number:");
 int n = sc.nextInt();
 
 for(int i = 1;i<=n;i++)
